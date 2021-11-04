8d4ddec23615996e422d74d7d2ee6cc77a6ef58e
Cyrena RAMDANI <cramdani@e3r7p3.42.fr>
2021-11-04 18:14:52 +0100

    exit [arg] return value works

--- srcs/executor/builtin/exit.c
+++ srcs/executor/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: cramdani <cramdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 18:22:37 by cramdani          #+#    #+#             */
-/*   Updated: 2021/10/31 13:45:19 by cramdani         ###   ########.fr       */
+/*   Updated: 2021/11/04 18:14:18 by cramdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,49 +18,73 @@
 */
 int	exit_atoi(char *str)
 {
-	int	i;
-	int	ret;
+	int			i;
+	long long	ret;
+	long long	neg;
+	char		*tmp;
 
-	i = 0;
+	tmp = ft_strtrim(str, " ");
+	i = tmp[0] == '-' || tmp[0] == '+';
 	ret = 0;
-	if (!str || str[0] == '-' || ft_strlen(str) > 3)
-		ret = 1;
-	else
+	neg = 1;
+	while (str && ft_isspace(tmp[i]))
+		i++;
+	if (tmp[0] == '-')
+		neg = -1;
+	while (tmp[i] && tmp[i] >= '0' && tmp[i] <= '9')
 	{
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			ret = ret * 10 + str[i] - '0';
-			i++;
-		}
+		ret = ret * 10 + tmp[i] - '0';
+		i++;
 	}
+	ret = ret * neg;
+	free(tmp);
 	return (ret % 256);
+}
+
+int	exit_isnumber(char	*nb)
+{
+	char	*tmp;
+	int		ret;
+
+	tmp = ft_strtrim(nb, " ");
+	if (tmp[0] == '-' || tmp[0] == '+')
+		ret = ft_isnumber(tmp + 1);
+	else
+		ret = ft_isnumber(tmp);
+	if (ret && (ft_strlen(tmp) > 20))
+		ret = 0;
+	free(tmp);
+	return (ret);
 }
 
 int	ft_exit(t_gen *data, t_lexer *cmd)
 {
-	data->exit_stat = 0;
+	int	is_num;
+
 	printf("exit\n");
 	if (cmd)
 	{
-		if (ft_isnumber(cmd->content) && cmd->next != NULL)
+		is_num = exit_isnumber(cmd->content);
+		if (is_num && cmd->next != NULL)
 		{
 			print_error("exit: too many arguments\n", NULL, NULL);
-			clean_lex(data->lex);
-			data->lex = NULL;
 			return (1);
 		}
-		else if (!ft_isnumber(cmd->content) || cmd->next != NULL)
+		else if (!is_num)
 		{
-			if (!ft_isnumber(cmd->content))
-				print_error("exit: ", cmd->content,": numeric argument required\n");
-			else
-				print_error("exit: too many arguments\n", NULL, NULL);
+			print_error("exit: ", cmd->content, ": numeric argument required\n");
 			data->exit_stat = 255;
+		}
+		else if (cmd->next != NULL)
+		{
+			print_error("exit: too many arguments\n", NULL, NULL);
+			data->exit_stat = 1;
 		}
 		else
 			data->exit_stat = exit_atoi(cmd->content);
 	}
 	exit(data->exit_stat);
 }
+	// printf("%d\n", data->exit_stat);
 	// clean_data(data);
 	// delete_data(data);
